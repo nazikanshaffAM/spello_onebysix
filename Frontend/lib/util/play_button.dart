c94fdd02c2185365ae10fe2c8ff2c9eaebac4e2c
VenujaBatheegama <159419891+VenujaBatheegama@users.noreply.github.com>
2025-02-19 23:41:09 +0530

    made the game page and the utils for game page

---
+++ Frontend/lib/util/play_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class PlayButton extends StatefulWidget {
+  Function onTap;
+  PlayButton({super.key, required this.onTap});
+
+  @override
+  State<PlayButton> createState() => _PlayButtonState();
+}
+
+class _PlayButtonState extends State<PlayButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFFFFC000),
+        shape: BoxShape.circle,
+        boxShadow: const [
+          BoxShadow(
+            color: Color(0xFFD29338),
+            offset: Offset(2, 4),
+            blurRadius: 0,
+          ),
+        ],
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          customBorder: const CircleBorder(),
+          splashColor: Colors.orange.withOpacity(0.5),
+          highlightColor: Colors.orange.withOpacity(0.7),
+          onTap: () {},
+          child: const Padding(
+            padding: EdgeInsets.all(12),
+            child: Icon(
+              Icons.play_arrow,
+              color: Colors.white,
+              size: 40,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
