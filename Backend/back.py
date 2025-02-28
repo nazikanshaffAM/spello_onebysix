adf05bb117af46cc0a3cd76e4104bc404a1d02cc
ozama-j <hushamajalal@gmail.com>
2025-02-28 11:36:18 +0530

    feat:model path

--- Backend/back.py
+++ Backend/back.py
@@ -1,8 +1,12 @@
 import os
 import json
 import random
-# import sounddevice as sd
 import vosk
 from rapidfuzz.distance import Levenshtein
 from flask import Flask, request, jsonify
 from io import BytesIO
+
+app = Flask(__name__)
+
+# path to the downloaded model
+MODEL_PATH = "vosk-model-small-en-us-0.15"
