267b8cc430b46bbd1060662e48490503adfeb384
ozama-j <hushamajalal@gmail.com>
2025-02-28 11:38:13 +0530

    feat: fixing model path

--- Backend/back.py
+++ Backend/back.py
@@ -10,3 +10,8 @@ app = Flask(__name__)
 
 # path to the downloaded model
 MODEL_PATH = "vosk-model-small-en-us-0.15"
+
+
+# Load Vosk Model
+if not os.path.exists(MODEL_PATH):
+    raise ValueError("Model not found! Please download and extract it.")
