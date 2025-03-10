f93bad3368bd5c079d3295b1459160c45c7f76d7
vizakan10 <visakan10@gmail.com>
2025-03-10 12:35:51 +0530

    feat: added basic main in login

---
+++ Frontend/lib/pages/LoginRelatedPages/login.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
