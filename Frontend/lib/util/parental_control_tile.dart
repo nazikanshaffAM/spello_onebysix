87bbf3cf583f801eca1b736eb236c7b9a56dd2b1
vizakan10 <visakan10@gmail.com>
2025-03-13 11:37:27 +0530

    feat:updated util/parent_control_tile

--- Frontend/lib/util/parental_control_tile.dart
+++ Frontend/lib/util/parental_control_tile.dart
@@ -1,21 +1,67 @@
 import 'package:flutter/material.dart';
 
 class ControlPanelTiles extends StatefulWidget {
-  const ControlPanelTiles(
-      {super.key, required this.tileName, this.isLocked = false});
+  const ControlPanelTiles({
+    super.key, 
+    required this.tileName, 
+    this.isLocked = false,
+    this.isSelected = false,  // Selection state passed from parent
+    this.onSelectionChanged,  // Callback to notify parent of selection changes
+  });
 
   final String tileName;
   final bool isLocked;
+  final bool isSelected;
+  final Function(bool)? onSelectionChanged;  // Add callback for selection changes
 
   @override
   State<ControlPanelTiles> createState() => _ControlPanelTilesState();
 }
 
 class _ControlPanelTilesState extends State<ControlPanelTiles> {
-  bool _isPressed = false; // Tracks button pressed state
+  late bool _isPressed; // Tracks button pressed state
+  
+  @override
+  void initState() {
+    super.initState();
+    _isPressed = widget.isSelected; // Initialize from isSelected prop
+    print("ControlPanelTiles initState: ${widget.tileName} - isSelected: ${widget.isSelected}, isLocked: ${widget.isLocked}");
+  }
+  
+  @override
+  void didUpdateWidget(ControlPanelTiles oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Only update local state if the prop changed from parent
+    if (widget.isSelected != oldWidget.isSelected) {
+      setState(() {
+        _isPressed = widget.isSelected;
+      });
+      print("ControlPanelTiles didUpdateWidget: ${widget.tileName} - isSelected changed to ${widget.isSelected}");
+    }
+  }
+
+  void _handlePress() {
+    // Only toggle if not locked or already selected
+    if (!widget.isLocked || _isPressed) {
+      setState(() {
+        _isPressed = !_isPressed; // Toggle pressed state
+      });
+      
+      // Notify parent component about the change
+      if (widget.onSelectionChanged != null) {
+        widget.onSelectionChanged!(_isPressed);
+        print("ControlPanelTiles: Notified parent that ${widget.tileName} is now $_isPressed");
+      }
+    } else {
+      print("ControlPanelTiles: ${widget.tileName} is locked and not selected, ignoring press");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    // Allow button to be pressed if it's not locked or if it's already selected
+    final bool isButtonEnabled = !widget.isLocked || _isPressed;
+    
     return Padding(
       padding: const EdgeInsets.only(left: 10.0, right: 10, top: 10, bottom: 5),
       child: AnimatedContainer(
@@ -23,7 +69,7 @@ class _ControlPanelTilesState extends State<ControlPanelTiles> {
         width: 150,
         height: 50,
         decoration: BoxDecoration(
-          color: widget.isLocked
+          color: !isButtonEnabled
               ? Colors.grey[300] // Disabled look when locked
               : _isPressed
                   ? const Color(0xFFFFC000)
@@ -31,7 +77,7 @@ class _ControlPanelTilesState extends State<ControlPanelTiles> {
           borderRadius: BorderRadius.circular(12),
           boxShadow: [
             BoxShadow(
-              color: widget.isLocked
+              color: !isButtonEnabled
                   ? Colors.grey // No shadow when locked
                   : _isPressed
                       ? const Color(0xFFD29338)
@@ -49,13 +95,7 @@ class _ControlPanelTilesState extends State<ControlPanelTiles> {
               borderRadius: BorderRadius.circular(12),
             ),
           ),
-          onPressed: widget.isLocked
-              ? null // Disable button when locked
-              : () {
-                  setState(() {
-                    _isPressed = !_isPressed; // Toggle pressed state
-                  });
-                },
+          onPressed: isButtonEnabled ? _handlePress : null,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -75,7 +115,7 @@ class _ControlPanelTilesState extends State<ControlPanelTiles> {
                 width: 30,
                 height: 30,
                 decoration: BoxDecoration(
-                  color: widget.isLocked
+                  color: !isButtonEnabled
                       ? Colors.grey // Greyed out when locked
                       : _isPressed
                           ? const Color(0xFF4C5679)
@@ -83,12 +123,12 @@ class _ControlPanelTilesState extends State<ControlPanelTiles> {
                   shape: BoxShape.circle,
                 ),
                 child: Icon(
-                  widget.isLocked
-                      ? Icons.lock // Show lock icon when locked
+                  !isButtonEnabled && !_isPressed
+                      ? Icons.lock // Show lock icon when locked and not selected
                       : _isPressed
                           ? Icons.check
                           : Icons.add,
-                  color: widget.isLocked
+                  color: !isButtonEnabled && !_isPressed
                       ? Colors.white // White lock icon when locked
                       : _isPressed
                           ? const Color(0xFFFFC000)
