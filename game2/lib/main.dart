007afde507270fa5e4fd31c296c3ffebe6da409c
raazi-afk <raazi.20233190@iit.ac.lk>
2025-03-24 05:07:49 +0530

    chore: added inline comments to main.dart

--- game2/lib/main.dart
+++ game2/lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'ui/start_screen.dart'; // Import the StartScreen
 
+// Main function to start the Hangman application
 void main() => runApp(HangmanApp());
 
+// Stateless widget representing the Hangman application
 class HangmanApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hangman',
-      debugShowCheckedModeBanner: false,
+      title: 'Hangman', // Set the application title
+      debugShowCheckedModeBanner: false, // Hide the debug banner
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blue, // Set the primary color theme
       ),
       home: StartScreen(), // Set the StartScreen as the initial screen
     );
