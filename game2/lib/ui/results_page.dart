7f04c4c246f6293deb80290a8a3cd5be45066be9
raazi-afk <raazi.20233190@iit.ac.lk>
2025-03-24 05:55:58 +0530

    feat: UI changes to results_page.dart

--- game2/lib/ui/results_page.dart
+++ game2/lib/ui/results_page.dart
@@ -22,13 +22,13 @@ class ResultPage extends StatelessWidget {
             // Display the average accuracy percentage
             Text(
               'Average Accuracy: ${averageAccuracy.toStringAsFixed(2)}%',
-              style: TextStyle(fontSize: 22), // Set font size
+              style: TextStyle(fontSize: 28), // Set font size
             ),
             SizedBox(height: 20), // Add space between elements
             // Display the number of lives left
             Text(
               'Lives Left: $livesLeft',
-              style: TextStyle(fontSize: 22), // Set font size
+              style: TextStyle(fontSize: 30), // Set font size
             ),
             SizedBox(height: 50), // Add space before the button
             ElevatedButton(
